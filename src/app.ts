14f50f9410744b91bf22873ccfd91da250301ef2
YgorRupp <ygor.rupp@hotmail.com>
2023-06-15 16:14:14 -0300

    add cors to app

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import { handleErrors } from "./errors/AppError";
 import { announcementRoutes } from "./routers/announcement.routes";
 import { usersRoutes } from "./routers/user.routes";
 import { loginRoutes } from "./routers/login.routes";
+import cors from "cors";
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/users", usersRoutes);
 app.use("/login", loginRoutes);
