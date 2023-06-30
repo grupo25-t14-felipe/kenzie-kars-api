f2bfcb957a16dbb2ea1f96a056c7b87bb9cdb6c9
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-06-30 11:14:24 -0400

    Fix: create address route

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use("/users", usersRoutes);
-app.use("/users/:id/address", addressRoutes);
+app.use("/address", addressRoutes);
 app.use("/login", loginRoutes);
 app.use("/announcements", announcementRoutes);
 app.use('/image', imageRoutes);
